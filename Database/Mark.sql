093819f54c04b43f4874130df85655f06eaa55ee
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-12 14:10:30 +0530

    Update Mark.sql

--- Database/Mark.sql
+++ Database/Mark.sql
@@ -2,7 +2,7 @@ CREATE TABLE Mark(
 Mark_ID INT NOT NULL PRIMARY KEY,
 Stu_ID INT,
 Subject_ID INT,
-Term_ID INT,
+Term_ID CHAR(4),
 Marks_obtained INT CHECK (Marks_obtained >= 0 AND Marks_obtained <= 100),
 FOREIGN KEY (Stu_ID) REFERENCES Student(Stu_ID),
 FOREIGN KEY (Subject_ID) REFERENCES Subject(Subject_ID),
