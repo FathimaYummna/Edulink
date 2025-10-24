ef4b7e5f68fe5084293adaa93a93b2eb2198249d
AmnaNisar2003 <amranisar369@gmail.com>
2025-10-24 10:14:27 +0530

    add admin login

---
+++ Backend/admindashboard.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+
+if (!isset($_SESSION['admin_username'])) 
+    {
+    header("Location: ../Frontend/adminlogin.html");
+    exit();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../Frontend/parent-login.css">
+</head>
+<body>
+    <div class="navbar">
+        <img src="../resources/icon.jpeg" alt="EduLink icon" class="icon">
+        <h1>gateway to effective student monitoring and management</span></h1>
+
+        <nav class="nav-links">
+            <a href="../Frontend/home.html">Home</a>
+            <a href="../Frontend/about.html">About</a>
+            <a href="../Frontend/account.html">Account</a>
+            <a href="../Frontend/support.html">Support</a>
+            <a href="logout.php">Logout</a>
+        </nav>
+
+    </div>
+<div class="dashboard">
+        <h2>Welcome back, <?php echo htmlspecialchars($_SESSION['admin_username']); ?>!</h2>
+        <button onclick="location.href='../Frontend/add_new_student.html'">Add New Student</button>
+</div>
+</body>
+<footer>
+    <p>&copy; 2024 EduLink. All rights reserved.</p>
+</footer>
+</html>
---
+++ Backend/home.js
@@ -0,0 +1,16 @@
+
+
+// Parent button click
+document.getElementById("parent").addEventListener("click", function() {
+    window.location.href = "parentlogin.html";
+});
+
+// Teacher button click
+document.getElementById("teacher").addEventListener("click", function() {
+    window.location.href = "teacherlogin.html";
+});
+
+// Admin button click
+document.getElementById("admin").addEventListener("click", function() {
+    window.location.href = "adminlogin.html";
+});
