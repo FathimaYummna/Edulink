faaa6efa41b740ffacd0d5d83131b3ef7432e468
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 23:20:41 +0530

    Change year_name type from VARCHAR to CHAR

--- Database/All_Sql_Queries.sql
+++ Database/All_Sql_Queries.sql
@@ -126,7 +126,7 @@ CREATE TABLE admin (
 CREATE TABLE student_subject(
 stu_id INT NOT NULL,
 sub_id INT NOT NULL,
-year_name VARCHAR(4) NOT NULL,
+year_name CHAR(4) NOT NULL,
 status ENUM('passed','following'),
 PRIMARY KEY (stu_id,sub_id,year_name),
 FOREIGN KEY (stu_id) REFERENCES student (stu_id),
