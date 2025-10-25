fe32b73ea2d827e31cc42f672f59e261dc9d3331
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 22:15:59 +0530

    Change created_at to use TIMESTAMP with default

--- Database/Comment.sql
+++ Database/Comment.sql
@@ -3,7 +3,7 @@ CREATE TABLE comment (
     stu_id INT NOT NULL,
     teach_id INT NOT NULL,
     comment VARCHAR(400) NOT NULL,
-    created_at DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (stu_id) REFERENCES student(stu_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
