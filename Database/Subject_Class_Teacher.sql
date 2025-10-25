06340bc6e555b8b129d468b517d6b5f775afbe87
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 21:36:50 +0530

    Increase role VARCHAR length from 20 to 40

--- Database/Subject_Class_Teacher.sql
+++ Database/Subject_Class_Teacher.sql
@@ -2,7 +2,7 @@ CREATE TABLE subject_class_teacher (
     teach_id INT,
     class_name INT,
     year_name INT,
-    role VARCHAR(20), 
+    role VARCHAR(40), 
     PRIMARY KEY (teach_id, class_name,year_name),
     FOREIGN KEY (teach_id) REFERENCES Teacher(teacher_id),
     FOREIGN KEY (class_name) REFERENCES Class(class_id),
