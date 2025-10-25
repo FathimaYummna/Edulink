5dd6dc75adf62fbbbdfbc1235de4e8a028cd9d41
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 14:50:46 +0530

    Rename table and update foreign key references

--- Database/Fee_Payment.sql
+++ Database/Fee_Payment.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Fee_Payment(
+CREATE TABLE fee_payment(
     stu_id INT NOT NULL,
     fee_id CHAR(4) NOT NULL,
     fee_status ENUM("Fully Paid", "Part Paid", "Unpaid"),
     amount DECIMAL(7,2),
-    month varchar(15),
-    FOREIGN KEY (Fee_ID) REFERENCES Fees(Fee_ID),
-    FOREIGN KEY (Stu_ID) REFERENCES Student(Stu_ID)
+    month VARCHAR(15),
+    FOREIGN KEY (fee_id) REFERENCES fee(fee_id),
+    FOREIGN KEY (stu_id) REFERENCES student(stu_id)
     );
   
