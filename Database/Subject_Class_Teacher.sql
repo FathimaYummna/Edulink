e83f8fa4317da949570d95f0e51bf6b08b6f9c4f
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 22:33:02 +0530

    Update foreign key references to lowercase
    Standardized foreign key references to lowercase.

--- Database/Subject_Class_Teacher.sql
+++ Database/Subject_Class_Teacher.sql
@@ -4,8 +4,8 @@ CREATE TABLE subject_class_teacher (
     year_name INT,
     role VARCHAR(40), 
     PRIMARY KEY (teach_id, class_name,year_name),
-    FOREIGN KEY (teach_id) REFERENCES Teacher(teacher_id),
-    FOREIGN KEY (class_name) REFERENCES Class(class_id),
-    FOREIGN KEY(year_name) REFERENCES Year (year_id)
+    FOREIGN KEY (teach_id) REFERENCES teacher(teacher_id),
+    FOREIGN KEY (class_name) REFERENCES class(class_id),
+    FOREIGN KEY(year_name) REFERENCES year(year_id)
 );
 
