a8b695640149ee8ef6ba813359ed73062cac4ec7
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 22:41:13 +0530

    Update subject_class_teacher table schema

--- Database/All_Sql_Queries.sql
+++ Database/All_Sql_Queries.sql
@@ -110,14 +110,14 @@ CREATE TABLE student_class (
     FOREIGN KEY(year_name) REFERENCES year(year_name)
 );
 CREATE TABLE subject_class_teacher (
-    teach_id INT,
-    class_name INT,
-    year_name INT,
-    role VARCHAR(40), 
+	teach_id INT,
+    class_name VARCHAR(10),
+    year_name CHAR(4), 
+    role VARCHAR(40),
     PRIMARY KEY (teach_id, class_name,year_name),
-    FOREIGN KEY (teach_id) REFERENCES teacher(teacher_id),
-    FOREIGN KEY (class_name) REFERENCES class(class_id),
-    FOREIGN KEY(year_name) REFERENCES year(year_id)
+    FOREIGN KEY (teach_id) REFERENCES teacher(teach_id),
+    FOREIGN KEY (class_name) REFERENCES class(class_name),
+    FOREIGN KEY(year_name) REFERENCES year (year_name)
 );
 CREATE TABLE admin (
     user_name varchar(50) primary key,
