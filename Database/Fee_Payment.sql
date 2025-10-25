4cb234866dcb9394cd734d96a6aec3530aae8176
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 15:07:16 +0530

    Update fee_payment table structure
    Modified the fee_payment table structure by making stu_id and fee_id nullable and added a primary key constraint.

--- Database/Fee_Payment.sql
+++ Database/Fee_Payment.sql
@@ -1,9 +1,10 @@
 CREATE TABLE fee_payment(
-    stu_id INT NOT NULL,
-    fee_id CHAR(4) NOT NULL,
-    fee_status ENUM("Fully Paid", "Part Paid", "Unpaid"),
+    stu_id INT,
+    fee_id CHAR(4),
+    fee_status ENUM('Fully Paid', 'Part Paid', 'Unpaid'),
     amount DECIMAL(7,2),
     month VARCHAR(15),
+    PRIMARY KEY(stu_id,fee_id),
     FOREIGN KEY (fee_id) REFERENCES fee(fee_id),
     FOREIGN KEY (stu_id) REFERENCES student(stu_id)
     );
