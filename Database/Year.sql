4b00c8bfcb3a7eb2a9260b3f6274283f188e930a
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 13:53:23 +0530

    Refactor Year table creation in SQL

--- Database/Year.sql
+++ Database/Year.sql
@@ -1,5 +1,3 @@
-USE edulink;
-CREATE TABLE Year (
-    year_id INT AUTO_INCREMENT PRIMARY KEY,
-    year_name VARCHAR(20) NOT NULL UNIQUE  
+CREATE TABLE year (
+    year_name CHAR(4) NOT NULL PRIMARY KEY  
 );
