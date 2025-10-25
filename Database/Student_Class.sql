bcc22c55efd4f80c8b1938f93ca91f74ca548d9c
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 15:16:04 +0530

    Modify student_class table

--- Database/Student_Class.sql
+++ Database/Student_Class.sql
@@ -1,8 +1,8 @@
 CREATE TABLE student_class (
     stu_id INT,
-    class_name INT,
-    year_name INT,
-    PRIMARY KEY (stu_id, class_id,year_name),
+    class_name VARCHAR(10),
+    year_name CHAR(4),
+    PRIMARY KEY (stu_id, class_name,year_name),
     FOREIGN KEY (stu_id) REFERENCES student(stu_id),
     FOREIGN KEY (class_name) REFERENCES class(class_name),
     FOREIGN KEY(year_name) REFERENCES year(year_name)
