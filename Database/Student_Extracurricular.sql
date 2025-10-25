4fcaf5b2b15806e0f84fedb88d01357f2b3a928f
yasasmi-subhagya <yasasmisubhagya2020@gmail.com>
2025-10-25 20:23:56 +0530

    Fix column type and add new columns in student_extracurricular

--- Database/Student_Extracurricular.sql
+++ Database/Student_Extracurricular.sql
@@ -1,7 +1,9 @@
 CREATE TABLE student_extracurricular (
     stu_id INT,
     activity_id INT,
-    duration varchar(50),
+    duration VARCHAR(50),
+    status VARCHAR(20),
+    value INT,
     PRIMARY KEY (stu_id, activity_id),
     FOREIGN KEY (stu_id) REFERENCES student(stu_id),
     FOREIGN KEY (activity_id) REFERENCES extracurricular(activity_id)
